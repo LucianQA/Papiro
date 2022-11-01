0f61f6c2ab51772e34ba1f58b8adc735e2d91b39
LucianQA <lucian.razec@gmail.com>
2022-11-01 08:36:12 +0200

    code cleanup

--- src/main/java/papetariePapiro/Papiro.java
+++ src/main/java/papetariePapiro/Papiro.java
@@ -25,37 +25,20 @@ public class Papiro {
         sketches.click();
         //WebElement blocA3 = chrome.findElement(By.cssSelector("body > div.premmerce-filter-ajax-container > div > div > div > div.main-content.col-lg-9.col-md-8.has-sidebar > ul > li.product-item.col-bg-4.col-lg-4.col-md-4.col-sm-4.col-xs-6.col-ts-12.style-16.product.type-product.post-6566.status-publish.first.instock.product_cat-bloc-pentru-desen-special-si-schite.has-post-thumbnail.taxable.shipping-taxable.purchasable.product-type-simple > div > div.product-info.equal-elem > h3 > a"));
         //blocA3.click();
-        wait(2000);
+        wait(3000);
         WebElement addToBasket1 = chrome.findElement(By.cssSelector("body > div.premmerce-filter-ajax-container > div > div > div > div.main-content.col-lg-9.col-md-8.has-sidebar > ul > li.product-item.col-bg-4.col-lg-4.col-md-4.col-sm-4.col-xs-6.col-ts-12.style-16.product.type-product.post-6566.status-publish.first.instock.product_cat-bloc-pentru-desen-special-si-schite.has-post-thumbnail.taxable.shipping-taxable.purchasable.product-type-simple > div > div.product-info.equal-elem > div > div.add-to-cart > a"));
         addToBasket1.click();
         wait(3000);
-
-        //Functia de add to favourites nu functioneaza
-        WebElement hartie = chrome.findElement(By.xpath("//*[@id=\"menu-cat-menu-1\"]/li[2]/a"));
-        hartie.click();
-        WebElement hartieCopiator = chrome.findElement(By.xpath( "//*[@id=\"menu-cat-menu-1\"]/li[2]/div/div[2]/div[1]/div/div/div/figure/a/img"));
-        hartieCopiator.click();
-        WebElement addToFavourites = chrome.findElement(By.xpath("//*[@id=\"menu-cat-menu-1\"]/li[2]/div/div[2]/div[1]/div/div/div/figure/a/img"));
-        addToFavourites.click();
-
-
-        //wait(2000);
-
-        WebElement favourites = chrome.findElement(By.cssSelector("#menu-item-11062 > a"));
-        favourites.click();
-        WebElement addToBasket = chrome.findElement(By.cssSelector("#yith-wcwl-row-6566 > td.product-add-to-cart > a"));
-        addToBasket.click();
-        wait(2000);
 
         //Cautare produs in bara de cautare si adaugare in cos
         WebElement searchField = chrome.findElement(By.cssSelector("#header > div.header-middle > div > div > div.header-control > div.block-search > div > form > div.form-content.search-box.results-search.instant-search-box > div > input"));
         searchField.click();
         searchField.sendKeys("Daco Px560");
-        wait(5000);
+        wait(3000);
         searchField.sendKeys(Keys.ENTER);
         WebElement addToBaket = chrome.findElement(By.cssSelector("#product-24139 > div.summary.entry-summary > form > button"));
         addToBaket.click();
-        wait(300);
+        wait(5000);
 
         //Verificare cos
         WebElement basket = chrome.findElement(By.cssSelector("#header > div.header-middle > div > div > div.header-control > div.block-minicart.kuteshop-mini-cart.kuteshop-dropdown > div.shopcart-dropdown.block-cart-link > a.link-dropdown.style1 > span.flaticon-cart01.icon"));
@@ -65,7 +48,7 @@ public class Papiro {
         WebElement increaseQuantity = chrome.findElement(By.cssSelector("body > main > div > div.row > div > div > div > form > table > tbody > tr.woocommerce-cart-form__cart-item.cart_item > td.product-quantity > div > div > a.btn-number.qtyplus.quantity-plus"));
         increaseQuantity.click();
         increaseQuantity.click();
-        wait(1000);
+        wait(3000);
         WebElement updateBasket = chrome.findElement(By.cssSelector("body > main > div > div.row > div > div > div > form > table > tbody > tr:nth-child(3) > td > button"));
         updateBasket.click();
         WebElement scroll = chrome.findElement(By.cssSelector("body"));
@@ -93,12 +76,14 @@ public class Papiro {
         WebElement street = chrome.findElement(By.cssSelector("#billing_address_1"));
         street.click();
         street.sendKeys("Str. Pacii Nr.2022");
+        wait(3000);
         WebElement streetDetails = chrome.findElement(By.cssSelector("#billing_address_2"));
         streetDetails.click();
         streetDetails.sendKeys("World Wide building");
         WebElement city = chrome.findElement(By.cssSelector("#billing_city"));
         city.click();
         city.sendKeys("Floresti");
+        wait(3000);
         WebElement county = chrome.findElement(By.cssSelector("#billing_postcode"));
         county.click();
         WebElement countyField = chrome.findElement(By.cssSelector("#select2-billing_state-container > span"));
@@ -131,13 +116,13 @@ public class Papiro {
         wait(3000);
         WebElement payCash = chrome.findElement(By.cssSelector("#payment > ul > li.wc_payment_method.payment_method_cod > label"));
         payCash.click();
-        wait(1000);
+        wait(3000);
         WebElement agreeTerms = chrome.findElement(By.cssSelector("#terms"));
         agreeTerms.click();
-        wait(1000);
+        wait(3000);
         WebElement backTop = chrome.findElement(By.cssSelector("body > a"));
         backTop.click();
-        wait(6000);
+        wait(4000);
 
         //Eliminarea produselor din cos si intoarcerea pe pagina principala
         WebElement homePage = chrome.findElement(By.cssSelector("#header > div.header-middle > div > div > div.logo > a > img"));
@@ -148,16 +133,16 @@ public class Papiro {
         checkEmptyBasket.click();
         WebElement removeB = chrome.findElement(By.cssSelector("body > main > div > div.row > div > div > div > form > table > tbody > tr:nth-child(2) > td.product-remove > a"));
         removeB.click();
-        wait(6000);
+        wait(4000);
         WebElement removeA = chrome.findElement(By.cssSelector("body > main > div > div.row > div > div > div > form > table > tbody > tr:nth-child(1) > td.product-remove > a"));
         removeA.click();
-        wait(6000);
+        wait(4000);
         WebElement backToShop = chrome.findElement(By.cssSelector("body > main > div > div.row > div > div > div > p.return-to-shop > a"));
         backToShop.click();
-        wait(5000);
+        wait(4000);
         WebElement home = chrome.findElement(By.cssSelector("#header > div.header-middle > div > div > div.logo > a > img"));
         home.click();
-        wait(8000);
+        wait(4000);
 
         //Inchidere browser
         chrome.close();
@@ -171,7 +156,7 @@ public class Papiro {
 
         private static void wait(int seconds){
             try{
-                Thread.sleep(5000);
+                Thread.sleep(seconds);
             }
             catch(InterruptedException e ){
                 e.printStackTrace();
